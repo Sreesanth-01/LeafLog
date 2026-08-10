3b76a2a097e848df5efd6ee499eb3f6529b65f60
Sreesanth-01 <iamshanth7@gmail.com>
2026-08-10 06:25:22 +0000

    react axios api client setup

--- reactapp/src/services/api.js
+++ reactapp/src/services/api.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import axios from 'axios'
+import { config } from 'karma'
 
-const api = () => {
-  return (
-    <div>api</div>
-  )
-}
+const api = axios.create({
+    baseURL:"http://8080-cdcaaaabedafdacceedbadfcfbabfcdecfafccfe.premiumproject.examly.io",
+    headers:{
+        "Content-Type":"application/json"
+    }
+})
+
+api.interceptors.request.use((config)=>{
+    const token = localStorage.getItem("token");
+    if(token){
+        config.headers.Authorization=`Bearer ${token}`;
+    }
+    return config;
+})
 
 export default api
