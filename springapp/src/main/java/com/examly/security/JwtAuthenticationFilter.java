74e810cbcc042eb7d9b10c3ef6baa47e39683443
Sreesanth-01 <iamshanth7@gmail.com>
2026-09-12 10:55:21 +0530

    feat: JwtAuthFilter

--- springapp/src/main/java/com/examly/security/JwtAuthenticationFilter.java
+++ springapp/src/main/java/com/examly/security/JwtAuthenticationFilter.java
@@ -1,14 +1,18 @@
 package com.examly.security;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -32,7 +36,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String token = authHeader.substring(7);
         UserDetails userDetails = userDetailsService.loadUserByUsername(jwtUtil.extractUserName(token));
         if(jwtUtil.isTokenValid(token,userDetails)){
+            String userEmail = jwtUtil.extractUserName(token);
 
+            UsernamePasswordAuthenticationToken authenticationToken = new  UsernamePasswordAuthenticationToken(userEmail,null,List.of());
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         }
+        filterChain.doFilter(request, response);
     }
 }
