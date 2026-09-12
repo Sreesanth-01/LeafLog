09c32c3657c96c56769da6239916735f24d0ee45
Sreesanth-01 <iamshanth7@gmail.com>
2026-09-12 11:32:30 +0530

    feat: Auth Controller

---
+++ springapp/src/main/java/com/examly/springapp/controller/AuthController.java
@@ -0,0 +1,57 @@
+package com.examly.springapp.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springapp.dto.LoginRequest;
+import com.examly.springapp.dto.LoginResponse;
+import com.examly.springapp.dto.SignUpRequest;
+import com.examly.springapp.dto.SignUpResponse;
+import com.examly.springapp.service.UserService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController 
+@RequestMapping("/api/auth")
+public class AuthController {
+     private final UserService userService;
+
+    AuthController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/signUp")
+    public ResponseEntity<Object> signUp(@RequestBody SignUpRequest signUpRequest){
+        try{
+            SignUpResponse response = userService.register(signUpRequest);
+            return ResponseEntity.ok(response.getMessage());
+        }
+        catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody LoginRequest loginRequest){
+        try{
+            LoginResponse loginResponse = userService.login(loginRequest);
+
+            Map<String,String> response = new HashMap<>();
+            response.put("token",loginResponse.getToken());
+            response.put("message",loginResponse.getMessage());
+            return ResponseEntity.ok(response);
+        }
+        catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+
+    }
+}
