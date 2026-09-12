c51b27c8f6dc99d8b9eb43bbb671e4dfbb79cc94
Sreesanth-01 <iamshanth7@gmail.com>
2026-09-12 11:12:59 +0530

    feat: Security Filter Chain

---
+++ springapp/src/main/java/com/examly/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.examly.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.examly.security.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration 
+@EnableWebSecurity 
+@RequiredArgsConstructor 
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.cors(Customizer.withDefaults())
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth.antMatchers("/api/auth/**").permitAll()
+            .anyRequest().authenticated())
+        .formLogin(form -> form.disable())
+        .httpBasic(Customizer.withDefaults())
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+
+    }
+}
